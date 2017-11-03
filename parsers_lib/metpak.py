22ea7872e0be1ff848966267651aa63db0beebb2
Ilan Mardix <ilan.mardix@gmail.com>
2017-11-03 14:29:52 -0500

    metpak wind_direction fix

--- parsers_lib/metpak.py
+++ parsers_lib/metpak.py
@@ -22,7 +22,10 @@ def metpak_parser(metpak_log, sensor_name, sensor_id):
 
             rest_of_the_line = line.split(']')[1].strip()
             fields = rest_of_the_line.split(',')
-            i = 0
+
+            data["wind_direction"] = get_winddirection(get_compass_file_name(metpak_log))
+
+            i = 2
             while (i < len(fields)):
                 data[headers[i]] = fields[i].strip()
                 i += 1
@@ -32,3 +35,28 @@ def metpak_parser(metpak_log, sensor_name, sensor_id):
 
     fo.close()
     return json_data
+
+def get_compass_file_name(metpak_log):
+    dirname = os.path.dirname(metpak_log)
+    filename = os.path.basename(metpak_log)
+    metpak_timestamp = filename.split('-')[-1].split(".")[0]
+    metpak_buoyname = filename.split('-')[2]
+    metpak_compass = "metpak-" + metpak_buoyname + "-compasswind_metpak-" + metpak_timestamp + ".txt"
+    return dirname + "/" + metpak_compass
+
+
+def get_winddirection(compassfile):
+    if not os.path.isfile(compassfile):
+        return ""
+
+    fo = open(compassfile)
+    for line in fo:
+        direction = line.split(' ')[4]
+
+    return direction
+
+
+#For debug purposes only
+if __name__ == "__main__":
+    example = "/home/ilan/sea/metpak-averaged-tabs225m09-201707301630.txt"
+    print(metpak_parser(example, "metpak", 45))
